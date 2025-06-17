6bc0cdee7b4c2c6ac26d5bd1b88a7657d3f4d9da
ryanuo <1577791638@qq.com>
2025-06-18 00:28:11 +0800

    refactor(app): update font assets path and move fonts to preview folder
    - Update font assets path in constants from '/assets/fonts/' to '/assets/preview-fonts/'
    - Move font files from public/assets/fonts to public/assets/preview-fonts
    - Add public/assets/fonts to .gitignore

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -10,26 +10,26 @@ export const fontList = [
   {
     name: 'Work Sans',
     value: 'Work-Sans',
-    assets: '/assets/fonts/Work-Sans.woff2',
+    assets: '/assets/font-preview/Work-Sans.woff2',
   },
   {
     name: '苹果方正',
     value: 'PingFang-Medium-sub',
-    assets: '/assets/fonts/PingFang-Medium-sub.ttf',
+    assets: '/assets/preview-fonts/PingFang-Medium-sub.ttf',
   },
   {
     name: '思源黑体',
     value: 'SourceHanSansSC-Regular',
-    assets: '/assets/fonts/20200203-111651-935f.woff',
+    assets: '/assets/preview-fonts/20200203-111651-935f.woff',
   },
   {
     name: 'Times New Roman',
     value: 'Times New Roman',
-    assets: '/assets/fonts/TimesNewRoman.ttf',
+    assets: '/assets/preview-fonts/TimesNewRoman.ttf',
   },
   {
     name: '阿里巴巴普惠体',
     value: 'AlibabaSans-Regular',
-    assets: '/assets/fonts/AlibabaSans-Regular.woff',
+    assets: '/assets/preview-fonts/AlibabaSans-Regular.woff',
   },
 ]
